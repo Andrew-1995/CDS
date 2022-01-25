b2fdc2aabc6fabe7e297fc31c7f6846533a3a5d9
Andrew-1995 <54816840+Andrew-1995@users.noreply.github.com>
2022-01-25 20:43:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask,request,jsonify
 import numpy as np
-import pickle
-
-
-import numpy as np
 import pandas as pd
-from basic_knn import knn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
 from sklearn.neighbors import KNeighborsClassifier
 
-from sklearn.neighbors import NearestNeighbors
 
 df = pd.read_csv('students_placement.csv')
 df.sample(5)
@@ -26,27 +20,17 @@ rf = RandomForestClassifier()
 rf.fit(X_train,y_train)
 y_pred = rf.predict(X_test)
 print(accuracy_score(y_test,y_pred))
-#save the model in pickle format
-#pickle.dump(knn,open('model.pkl','wb'))
 
-#knn = NearestNeighbors(10)
 
 knn = KNeighborsClassifier(n_neighbors=10)
 knn.fit(X, y)
 
-#knn.fit(y_pred)
 
 # Its important to use binary mode
 knnPickle = open('model.pkl', 'wb')
 
 # source, destination
 pickle.dump(knn, knnPickle)
-
-
-# load the model from disk
-#loaded_model = pickle.load(open('model.pkl', 'rb'))
-#result = loaded_model.predict(X_test)
-
 
 
 
